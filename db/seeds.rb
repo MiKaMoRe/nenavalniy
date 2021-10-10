6011f479e3cf7949aeb8e897f941a0869cf0b738
r.kim <r.kim@sftpro.ru>
2021-10-11 00:54:38 +0300

    Product#index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create!([
+  {
+    article: 'AA-31',
+    name: 'Балдеж Полетеж',
+    price: 20000,
+    amount: 10
+  },
+  {
+    article: 'AA-240',
+    name: 'Балдеж Низкополетеж',
+    price: 10000,
+    amount: 100
+  },
+  {
+    article: 'YRT-124',
+    name: 'Балдежный Балдеж',
+    price: 3000,
+    amount: 300
+  },
+  {
+    article: 'GNH-1',
+    name: 'Летный Балдеж',
+    price: 7000,
+    amount: 0
+  }
+])
