dc1068524da1b98d535891d3caec8e1a64e9896c
r.kim <r.kim@sftpro.ru>
2021-10-21 13:52:57 +0300

    Add cart features tests. Update product index spec

--- spec/features/product/index_spec.rb
+++ spec/features/product/index_spec.rb
@@ -7,16 +7,36 @@ feature 'User can see product list', "
   As a guest
   I'd like to be able see product list
 " do
-  describe 'Guest' do
+  describe 'when guest' do
     given!(:product) { create(:product) }
 
-    scenario 'Open products list' do
+    scenario 'when open products list' do
       visit products_path
 
-      expect(page).to have_content product.article
       expect(page).to have_content product.name
       expect(page).to have_content product.price
       expect(page).to have_content product.amount
+    end
+  end
+
+  describe 'when product' do
+    given(:product_out_of_stock) { create(:product, :out_of_stock) }
+    given(:product) { create(:product) }
+
+    scenario 'in stock' do
+      product
+      visit products_path
+
+      expect(page).to have_content 'В корзину'
+      expect(page).to have_content 'В наличии'
+    end
+
+    scenario 'out of stock' do
+      product_out_of_stock
+      visit products_path
+
+      expect(page).to have_content 'Нет в наличии'
+      expect(page).to_not have_content 'В корзину'
     end
   end
 end
